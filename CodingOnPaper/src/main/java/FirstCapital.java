9d9ba9c89e086c7b218ff594ff109092fb015b7f
Máté Jónás <kornjoni@gmail.com>
2017-11-22 13:15:20 +0100

    Capitalizes words along the separating character

---
+++ CodingOnPaper/src/main/java/FirstCapital.java
@@ -0,0 +1,25 @@
+public class FirstCapital {
+
+    /**
+     * Capitalizes words along the separating character
+     * @param name the String we want to change
+     * @param separator
+     * @return the capitalized String
+     */
+    public String firstCapital(String name, String separator) {
+        StringBuilder result = new StringBuilder();
+        String[] splitted = name.toLowerCase().split(separator);
+        for (String line : splitted) {
+            if (!line.equals("")) {
+                char up = Character.toUpperCase(line.charAt(0));
+                result.append(line.replace(line.charAt(0), up)).append(" ");
+            }
+        }
+        return result.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FirstCapital().firstCapital("JOHN.....dOe", "\\."));
+    }
+
+}
